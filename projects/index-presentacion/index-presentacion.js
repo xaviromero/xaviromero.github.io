05e96e6512ad308c12330551ebae8613f9a0c66c
xaviromero <82179332+xaviromero@users.noreply.github.com>
2021-05-26 15:45:21 +0200

    presentacion update

---
+++ projects/index-presentacion/index-presentacion.js
@@ -0,0 +1,6 @@
+(function(Saola) {
+	var docData = {"ascene":{"aa":true,"autoKeyTime":0,"currentTime":0,"id":1,"k":8,"n":"A través de la capa de escena","rp":[[0,0,{"f":[0],"fe":[0,0,100,100,100,0,0,0],"l":["none"],"pso":[50,50,1200,1],"r":[0,0,"100%","100%",0]}]],"sceneLayer":1,"st":[0,0],"timelines":[{"dur":0,"id":1,"n":"Línea de tiempo"}]},"dur":0,"h":800,"k":80896,"mm":[0,0],"mpv":2,"resourceFolder":"resources","resources":[],"scenes":[{"aa":true,"autoKeyTime":0,"currentTime":0,"id":1,"k":8,"n":"Scene_1","rp":[[0,0,{"f":[1,255,255,255,255],"fe":[0,0,100,100,100,0,0,0],"l":["none"],"pso":[50,50,1200,1],"r":[0,0,1240,800,0]}]],"sceneLayer":0,"st":[0,0],"timelines":[{"dur":0,"id":1,"n":"Línea de tiempo"}]}],"v":5,"w":1240};
+	var f = [];
+	docData.f = f;
+	Saola.loadedDocs.push(docData);
+}(AtomiSaola)); 
