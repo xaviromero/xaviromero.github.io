5421388518e79c2548be6a47a9e9c19dc6b27223
Xavier Romero <nk___@hotmail.com>
2022-08-14 16:54:02 +0200

    work update

---
+++ room-26-resources/mainjs.js
@@ -0,0 +1,11 @@
+var world = document.getElementById("boxDiv");
+        var x;
+        var y;
+
+        document.addEventListener('mousemove', function(e)
+        {
+            x = e.clientX;
+            y = e.clientY;
+            y = -y;
+            world.style.transform = "translateZ(600px) rotateX("+y+"deg) rotateY("+x+"deg)";
+        });
